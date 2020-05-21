4a431268fcfad0e4388e3f1e9270acb152f3696a
hanmihyeon <ilikeyou2169@gmail.com>
2020-05-21 15:34:32 +0900

    Updates

---
+++ precache-manifest.b66c45636317de7482b858b86835d591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01428894cc48431720f1ea9b0f019a41",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "41899df07af76dcbd992",
+    "url": "/movie_app/static/css/main.43fa7d15.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41899df07af76dcbd992",
+    "url": "/movie_app/static/js/main.7659d897.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
